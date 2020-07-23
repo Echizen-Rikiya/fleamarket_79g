4393ebaaccfb04d051933092ca17e990ad57d776
k-shizukuda <k.shizukuda@icloud.com>
2020-07-23 10:29:09 +0900

    create-usersDatabase-adressesDatabase

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
   
-  validates :email, format: {with: /\A[a-zA-Z0-9_#!$%&`'*+-{|}~^\/=?.]+@[a-zA-Z0-9][a-zA-Z0-9.-]+\z/}
+  validates :email,
+   format: { with: /\A[a-zA-Z0-9_#!$%&`'*+-{|}~^\/=?.]+@[a-zA-Z0-9][a-zA-Z0-9.-]+\z/},
+   presence: true
   validates :password, format: {with: /[a-z\d]{7,}/i} #アルファベット、７文字以上
   validates :first_name, :family_name, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角ひらがなカタカナ漢字
   validates :kana_family_name, :kana_first_name, format: {with: /\A[ぁ-んー－]+\z/} #全角ひらがわ
--- db/migrate/20200722033154_devise_create_users.rb
+++ db/migrate/20200722033154_devise_create_users.rb
@@ -4,8 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
+      t.string :family_name,        null: false
+      t.string :first_name,         null: false
+      t.string :kana_family_name,   null: false
+      t.string :kana_first_name,    null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.integer :birth_year,        null: false
+      t.integer :birth_month,       null: false
+      t.integer :birth_day,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
